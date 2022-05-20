8cf89f19368222bfd5ab275699eddb4b73dac2f4
sgmartino-modo <sergio.martino@modo.com.ar>
2022-05-20 04:32:49 -0300

    custom hook de un contador

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { CounterApp } from './components/01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <CounterApp />
+  <CounterWithCustomHook />
 );
 
